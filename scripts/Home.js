23d5fc89b793617648bcd17546bdd53e3855f58d
Samarthsteve <samarth9190sasteve@gmail.com>
2024-06-22 21:17:49 +0530

    Completed Upper Hero

---
+++ scripts/Home.js
@@ -0,0 +1,62 @@
+gsap.to("#hero__bg", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				scale: 1.5
+});
+
+gsap.to("#hero__man", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				scale: 0.5
+});
+
+gsap.to("#hero__mount-left", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				x: -500
+});
+
+gsap.to("#hero__mount-right", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				x: 500
+});
+
+gsap.to("#hero__mount-left", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				x: -500
+});
+
+gsap.to("#hero__mount-left", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				x: -500
+});
+
+gsap.to("#hero__clouds1", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				x: 200
+});
+
+gsap.to("#hero__clouds2", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				x: -200
+});
+
+gsap.to("#hero__main-text", {
+				scrollTrigger: {
+								scrub: 0.5
+				},
+				y: 500
+});
--- scripts/Navbar.js
+++ scripts/Navbar.js
@@ -2,15 +2,15 @@ let toggle_nav = document.querySelector(".nav__toggle");
 
 let nav = document.querySelector("nav");
 
-toggle_nav.onclick = function() {
-    toggle_nav.classList.toggle("toggle__active");
-    nav.classList.toggle("nav__active");
-}
+toggle_nav.onclick = function () {
+  toggle_nav.classList.toggle("toggle__active");
+  nav.classList.toggle("nav__active");
+};
 
 nav.onmouseenter = function () {
-    toggle_nav.classList.add("hide");
-}
+  toggle_nav.classList.add("hide");
+};
 
 nav.onmouseleave = function () {
-    toggle_nav.classList.remove("hide");
-}
+  toggle_nav.classList.remove("hide");
+};
