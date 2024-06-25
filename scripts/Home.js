72bfa5aa3c1bed03036bd8db7a9d1bbf1a846a66
Samarth Kotian <samarth9190sasteve@gmail.com>
2024-06-25 01:12:03 +0000

    Experimental commit for hero

--- scripts/Home.js
+++ scripts/Home.js
@@ -16,47 +16,48 @@ gsap.to("#hero__mount-left", {
 				scrollTrigger: {
 								scrub: 0.5
 				},
-				x: -500
+				xPercent: -100
 });
 
 gsap.to("#hero__mount-right", {
 				scrollTrigger: {
 								scrub: 0.5
 				},
-				x: 500
-});
-
-gsap.to("#hero__mount-left", {
-				scrollTrigger: {
-								scrub: 0.5
-				},
-				x: -500
-});
-
-gsap.to("#hero__mount-left", {
-				scrollTrigger: {
-								scrub: 0.5
-				},
-				x: -500
+				xPercent: 100
 });
 
 gsap.to("#hero__clouds1", {
 				scrollTrigger: {
 								scrub: 0.5
 				},
-				x: 200
+				xPercent: 50
 });
 
 gsap.to("#hero__clouds2", {
 				scrollTrigger: {
 								scrub: 0.5
 				},
-				x: -200
+				xPercent: -50
 });
 
-gsap.to("#hero__main-text", {
-				scrollTrigger: {
-								scrub: 0.5
-				},
-				y: 500
-});
+// gsap.to("#hero__main-text", {
+// 				scrollTrigger: {
+// 								scrub: 0.5
+// 				},
+// 				yPercent: 550
+// });
+
+
+
+gsap.fromTo("#hero__main-text", 
+						{
+						yPercent: 180,
+							xPercent: 10,
+							
+}, {
+							duration: 1,
+							
+							y: "75vh",
+							delay: 5
+							
+})
